1e10095795e7c00ff28bcae46c4137788b3fcaf3
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2019-05-21 17:36:01 -0300

    Start a new way to gen

---
+++ defs/GtkTreeStore.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+	'configs' => [
+		'name'=>"GdkColorspace_",
+		'extends'=>"Php::Base",
+	],
+
+	'dependences' => [
+
+	],
+
+	'methods' => [
+
+
+		// 
+		'__construct' => [
+			'is_constructor' => TRUE
+			'cpp-method' => "gtk_dialog_new",
+			'return-type' => NULL,
+			'parameters' => NULL,
+		],
+
+
+		// 
+
+
+	],
+
+];
